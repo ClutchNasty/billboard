86dcc18124c325c2bcb536173ebfe7c4930077d8
Devyn Gish <devyngish@gmail.com>
2019-08-22 17:58:37 -0600

    first push

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  get 'static_pages/home'
+  get 'static_pages/about'
+  root 'static_pages#home'
+  resources :billboards do
+    resources :songs
+  end
+  resources :artists do
+    resources :songss
+  end
+  end
